c31153ffcc6800ab517e5fb6e88d4c1169ea3e14
wjq <w@w>
2024-05-10 14:40:05 +0200

    default config

--- install_dependency.sh
+++ install_dependency.sh
@@ -10,3 +10,6 @@ sudo apt-get install -y python3-dev python3-venv git build-essential
 cd kAFL
 make deploy
 
+# vim ./kAFL/kafl/fuzzer/kafl_fuzzer/common/config/default_settings.yaml
+# qemu_base: -enable-kvm -machine kAFL64-v1 -cpu kAFL64-Hypervisor-v1,+vmx -no-reboot  -display none -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device e1000,netdev=mynet0,mac=52:55:00:d1:55:01
+# #qemu_append: nokaslr oops=panic nopti mitigations=off console=ttyS0
